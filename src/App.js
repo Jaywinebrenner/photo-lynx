646939c9abe02badd998e2c33ab5ea32dc277d2c
jay winebrenner <jaywinebrenner@jays-iMac-2.local>
2020-09-08 14:28:59 -0700

    write modal

--- src/App.js
+++ src/App.js
@@ -1,37 +1,110 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import Logo from './media/logo.png'
+import './App.css';
 import Post from './Post';
-import { db } from './firebase'
+import { db } from './firebase';
+import {makeStyles} from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle)
 
   const [posts, setPosts] = useState([])
+  const [open, setOpen] = useState(false)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const [userName, setUserName] = useState("");
 
 
   // useEffect runs a piece of code based on a specific condiction
 
   useEffect(() => {
+
     db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => doc.data()))
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })))
     })
 
-    console.log("DB", db);
   }, []);
+
+  const signUp = (e) => {
+
+  }
 
 
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <img className="app__headerImage" src={Logo} />
+            <Input
+              placeholder="userName"
+              type="text"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </center>
+        </div>
+      </Modal>
+
       {/* HEADER */}
       <div className="app__header">
         <img className="app__headerImage" src={Logo} />
       </div>
 
-      {posts.map(post => 
-          <Post userName={post.userName} imageUrl={post.imageUrl} caption={post.caption}/>
-      )}
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
-
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          userName={post.userName}
+          imageUrl={post.imageUrl}
+          caption={post.caption}
+        />
+      ))}
     </div>
   );
 }
