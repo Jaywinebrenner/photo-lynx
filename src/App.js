4dd34d4efbdbd7f902838ec46849581573f1aac8
jay winebrenner <jaywinebrenner@jays-iMac-2.local>
2020-09-08 10:59:14 -0700

    configure firestore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Logo from './media/logo.png'
-import Post from './Post'
+import Post from './Post';
+import { db } from './firebase'
 
 function App() {
+
+  console.log("db", db);
+  const [posts, setPosts] = useState([])
+
+
+  // useEffect runs a piece of code based on a specific condiction
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    })
+  }, []);
+
+
   return (
     <div className="app">
-    
       {/* HEADER */}
       <div className="app__header">
         <img className="app__headerImage" src={Logo} />
       </div>
 
+      {posts.map(post => 
+          <Post userName={post.userName} imageUrl={post.imageUrl} caption={post.caption}/>
+      )}
 
-      {/* POSTS */}
-      <Post/>
-
-
-      {/* POSTS */}
 
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -2,23 +2,18 @@ import React from 'react';
 import './Post.css';
 import Avatar from "@material-ui/core/Avatar"
 
-const Post = () => {
+const Post = ({imageUrl, userName, caption}) => {
   return (
     <div className="post">
       <div className="post__header">
         <Avatar className="post__avatar" src="media/dracula.png" />
-      <h3>Username</h3>
+        <h3>{userName}</h3>
       </div>
-      <img
-        className="post__image"
-        src="https://www.inspiredtaste.net/wp-content/uploads/2018/10/Homemade-Vegetable-Soup-Recipe-2-1200.jpg"
-      />
-    
+      <img className="post__image" src={imageUrl} />
+
       <h4 className="post__text">
-        <strong>Count Dracula:</strong> Check out my soup!
+        <strong>{userName}:</strong> {caption}
       </h4>
-
-
     </div>
   );
 }
